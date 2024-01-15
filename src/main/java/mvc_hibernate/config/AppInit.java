4732a22061455e180881bf4ded864d2090bdde83
magomedov-ali <magomedov.ali.2000@gmail.com>
2024-01-15 03:52:15 +0300

    first commit for 2.3.1

--- src/main/java/mvc_hibernate/config/AppInit.java
+++ src/main/java/mvc_hibernate/config/AppInit.java
@@ -1,26 +1,21 @@
-package web.config;
+package mvc_hibernate.config;
 
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
 
-    // Метод, указывающий на класс конфигурации
     @Override
     protected Class<?>[] getRootConfigClasses() {
         return null;
     }
 
-
-    // Добавление конфигурации, в которой инициализируем ViewResolver, для корректного отображения jsp.
     @Override
     protected Class<?>[] getServletConfigClasses() {
         return new Class<?>[]{
-                WebConfig.class
+                JavaConfig.class
         };
     }
 
-
-    /* Данный метод указывает url, на котором будет базироваться приложение */
     @Override
     protected String[] getServletMappings() {
         return new String[]{"/"};
---
+++ src/main/java/mvc_hibernate/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package mvc_hibernate.repositories;
+
+import mvc_hibernate.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
